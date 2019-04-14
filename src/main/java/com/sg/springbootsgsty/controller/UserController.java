d125ff9d02926dba0eed5c1e18ede720a4dc3571
shaogangzi <503495237@qq.com>
2019-04-14 20:47:30 +0800

    swagger快速构建文档

--- src/main/java/com/sg/springbootsgsty/controller/UserController.java
+++ src/main/java/com/sg/springbootsgsty/controller/UserController.java
@@ -2,6 +2,9 @@ package com.sg.springbootsgsty.controller;
 
 import com.sg.springbootsgsty.dao.UserDao;
 import com.sg.springbootsgsty.entity.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/user/")
 @Slf4j
+@Api(value = "用户服务类")
 public class UserController {
 
     @Autowired
@@ -18,16 +22,20 @@ public class UserController {
 
 
     @RequestMapping("lists")
+    @ApiOperation(value = "获取所有的用户列表",notes = "获取所有的用户列表")
     public List<User> list(){
         return userDao.findAll();
     }
 
     @GetMapping("list/{id}")
+    @ApiOperation(value = "通过id来查询用户",notes = "通过id来查询用户")
+    @ApiImplicitParam(name="id" ,value = "id" ,required = true,dataType = "Integer")
     public User listByid(@PathVariable("id") Integer id){
         return userDao.findById(id).get();
     }
 
     @PostMapping(value = "add")
+    @ApiImplicitParam(name="user" ,value = "user" ,required = true,dataType = "User")
     public List<User> add(User user){
         User save = userDao.save(user);
         return userDao.findAll();
