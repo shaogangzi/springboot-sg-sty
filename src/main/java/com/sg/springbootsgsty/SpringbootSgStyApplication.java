feaabf72f9403b2230b5b2f0aac0019b0235c50e
shaogangzi <503495237@qq.com>
2019-04-14 12:13:24 +0800

    Springboot学习笔记

---
+++ src/main/java/com/sg/springbootsgsty/SpringbootSgStyApplication.java
@@ -0,0 +1,13 @@
+package com.sg.springbootsgsty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSgStyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSgStyApplication.class, args);
+	}
+
+}
