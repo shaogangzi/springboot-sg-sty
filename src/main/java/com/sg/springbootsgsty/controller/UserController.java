6aa550384886f68be88855a750dad1e08971f472
shaogangzi <503495237@qq.com>
2019-04-21 20:35:21 +0800

    '参数获取'

--- src/main/java/com/sg/springbootsgsty/controller/UserController.java
+++ src/main/java/com/sg/springbootsgsty/controller/UserController.java
@@ -2,10 +2,10 @@ package com.sg.springbootsgsty.controller;
 
 import com.sg.springbootsgsty.bean.User;
 import com.sg.springbootsgsty.service.UserService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,23 +13,32 @@ import java.util.List;
 @RequestMapping("/user")
 public class UserController {
 
+    //http://localhost:port/name/swagger-ui.html
+
     @Autowired
     UserService userService;
 
 
-    @RequestMapping("/{id}")
+    @GetMapping(value="/{id}")
+    @ApiOperation(value = "通过Id查询用户" ,notes = "查询用户")
+    @ApiImplicitParam(name = "id" ,value = "用户id",required = true,dataType = "Long",paramType ="path")
     public User getUser(@PathVariable Long id){
+        System.out.println("---------"+id);
         return userService.selectUserById(id);
     }
 
-    @RequestMapping("/list")
-    public List<User> listUsers(User user){
+    @GetMapping("/list")
+    @ApiOperation(value = "批量查询用户",notes = "更加条件查询用户")
+    @ApiImplicitParam(name = "user",value = "用户信息",required = true,dataType = "User")
+    public List<User> listUsers(@RequestBody User user){
         return userService.selectUsersByCondition(user);
     }
 
-    @RequestMapping("/add")
-    public int addUser(User user){
-        return userService.addUser(user);
+    @PutMapping("/add")
+    @ApiOperation(value = "新增用户",notes = "新增用户呀")
+    @ApiImplicitParam(name = "userinfo",value = "新增的用户信息",required = true,dataType = "User")
+    public int addUser(@RequestBody User userinfo){
+        return userService.addUser(userinfo);
     }
 
 
